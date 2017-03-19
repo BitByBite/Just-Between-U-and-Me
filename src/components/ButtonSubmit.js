a19c6d8e7d1f821deaa31deef142f042df793dda
Sussbus <jacobsussman5@gmail.com>
2017-03-19 15:33:29 -0500

    implemented the new loading animation

--- src/components/ButtonSubmit.js
+++ src/components/ButtonSubmit.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
 
-import spinner from '../images/loading2.gif';
+import spinner from '../images/ring.gif';
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
