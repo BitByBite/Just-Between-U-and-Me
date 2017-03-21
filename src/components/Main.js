21d01ff5e014c9763051223ee349678904fa8ca0
SeanM <smokarry199@gmail.com>
2017-03-20 19:41:33 -0500

    idk if saved last time

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,28 +6,28 @@ import Relationships from './Relationships';
 import Ethnicity from './Ethnicity';
 
 export default class Main extends Component {
-  render() {
-	  return (
-	    <Router>
-	      <Scene key="root">
-	        <Scene key="loginScreen"
-	          component={LoginScreen}
-	        	animation='fade'
-	          hideNavBar={true}
-	          initial={true}
-	        />
-	        <Scene key="relationShips"
-	          component={Relationships}
-	          animation='fade'
-	          hideNavBar={true}
-	        />
-            <Scene key="ethnicity"
-                component={Ethnicity}
-                animation='fade'
-                hideNavBar={true}
-            />
-	      </Scene>
-	    </Router>
-	  );
+	render() {
+		return (
+			<Router>
+				<Scene key="root">
+					<Scene key="loginScreen"
+						component={LoginScreen}
+						animation='fade'
+						hideNavBar={true}
+						initial={true}
+					/>
+					<Scene key="relationShips"
+						component={Relationships}
+						animation='fade'
+						hideNavBar={true}
+					/>
+					<Scene key="ethnicity"
+						component={Ethnicity}
+						animation='fade'
+						hideNavBar={true}
+					/>
+				</Scene>
+			</Router>
+		);
 	}
 }
