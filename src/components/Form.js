5abc36e72bdec251bcd15b03bf4265649778484f
youngjin17 <youngjin.w.kim@gmail.com>
2017-03-21 17:07:20 -0500

    some stuff didnt go through in first push

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,7 +11,6 @@ import {
 } from 'react-native';
 
 import UserInput from './UserInput';
-import ButtonSubmit from './ButtonSubmit';
 import SignupSection from './SignupSection';
 
 import usernameImg from '../images/username.png';
