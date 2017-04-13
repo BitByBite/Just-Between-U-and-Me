ce67af6981e5e3c1f498cb4487058c44d081d47c
phatty21 <nsilich19@gmail.com>
2017-04-13 08:36:21 -0500

    Ask page is fixed, but the bottom nav is missing rn, ill add later cause it was causing issues

---
+++ src/components/AnswerPage.js
@@ -0,0 +1,162 @@
+
+import React, { Component } from 'react';
+import { Text, View, TextInput, Button, Image, TouchableOpacity } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+
+
+export default class AnswerPage extends Component {
+  constructor(props) {
+      super(props);
+      this.state = ({
+          question: '',
+          description: '',
+          selectedTab: ''
+
+      });
+  }
+  onPressButton() {
+
+  }
+  render() {
+      return (
+        <View style={styles.backdrop}>
+          <View style={styles.dropdown}>
+            <View style={styles.selectors}>
+              <View style={styles.plsWork}>
+                <ModalDropdown textStyle={styles.selectorText} defaultValue={'Categories  ∨'} options={['Crippling Depression', 'Osteoperosis']} />
+              </View>
+            </View>
+          <View style={styles.selectors}>
+            <View style={styles.plsWork}>
+              <ModalDropdown textStyle={styles.selectorText} defaultValue={'Receiver  ∨'} options={['Edups', 'dade', 'kysFag', 'nigger', 'sandNigger']} />
+            </View>
+          </View>
+          <View>
+            <TouchableOpacity activeOpacity={0.4} onPress={this.onPressButton}>
+              <View style={styles.plsWork}>
+                <Text style={styles.addTags}>Add Tags</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.bottomBox}>
+          <View style={styles.bottom}>
+            <View style={styles.views}>
+              <TextInput
+                  style={styles.smallText}
+                  placeholder='Question Title'
+                  returnKeyType='next'
+                  placeholderTextColor='#BABABA'
+                  underlineColorAndroid='transparent' />
+            </View>
+            <View style={styles.views}>
+              <TextInput
+                  style={styles.largeText}
+                  placeholder='Description'
+                  returnKeyType='next'
+                  placeholderTextColor='#BABABA'
+                  underlineColorAndroid='transparent'
+                  multiline={true} />
+              </View>
+            <View style={styles.button}>
+              <Button color={'#BABABA'} title={'Ask Question'} onPress={this.onPressButton} />
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = {
+    backdrop: {
+        backgroundColor: '#F3F3F3',
+        flex: 1,
+    },
+    logo: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'black',
+        fontFamily: 'Avenir',
+        fontSize: 17
+    },
+    textBold: {
+        color: 'black',
+        fontFamily: 'Avenir-Heavy',
+        fontSize: 17,
+        fontWeight: 'bold'
+    },
+    bottom: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
+    bottomBox: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        backgroundColor: '#F3F3F3',
+    },
+    views: {
+        paddingTop: 15,
+        paddingLeft: 12,
+        paddingRight: 12,
+        //Also padding-bottom can be added too
+        // This can be changed to add a divider between the boxes; change to E5E5E5
+        borderBottomColor: 'white',
+        borderBottomWidth: 1,
+    },
+    button: {
+        paddingBottom: 10,
+        paddingTop: 15
+    },
+    smallText: {
+        height: 50,
+        backgroundColor: '#E5E5E5',
+        borderWidth: 2,
+        borderColor: '#E5E5E5',
+        borderRadius: 5,
+        paddingRight: 25,
+        paddingLeft: 25,
+        textAlign: 'center',
+
+    },
+    largeText: {
+        height: 300,
+        backgroundColor: '#E5E5E5',
+        borderWidth: 2,
+        borderColor: '#E5E5E5',
+        borderRadius: 5,
+        paddingLeft: 25,
+        fontSize: 15
+    },
+    dropdown: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 50,
+        flex: 0
+    },
+    selectors: {
+      margin: 8,
+    },
+    selectorText: {
+      fontFamily: 'Avenir',
+      color: '#BABABA',
+      fontSize: 14,
+      margin: 4
+    },
+    plsWork: {
+      borderRadius: 20,
+      backgroundColor: 'white',
+      paddingLeft: 15,
+      paddingRight: 15,
+    },
+    addTags: {
+      fontSize: 14,
+      fontFamily: 'Avenir',
+      color: '#BABABA',
+      padding: 3.5
+    }
+}
--- src/components/App/Ask.js
+++ src/components/App/Ask.js
@@ -4,6 +4,8 @@ import BottomNav from './BottomNav';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 import TabNavigator from 'react-native-tab-navigator';
 import Header from '../Header';
+import AnswerPage from '../AnswerPage';
+import AskPage from '../AskPage';
 
 export default class Ask extends Component {
     constructor(props) {
@@ -24,47 +26,10 @@ export default class Ask extends Component {
                 <Header
                     text={<Text style={styles.text}>Just Between <Text style={styles.textBold}>U</Text> and <Text style={styles.textBold}>Me</Text></Text>}
                 />
-                <View style={styles.top}>
-                    <View style={styles.bar}>
-                        <TabNavigator>
-                            <TabNavigator.Item
-                                selected={this.state.selectedTab === 'Ask'}
-                                title=" "
-                                renderIcon={() => <Text>Ask</Text>}>
-                            </TabNavigator.Item>
-                            <TabNavigator.Item
-                                selected={this.state.selectedTab === 'Answer'}
-                                title=" "
-                                renderIcon={() => <Text>Answer</Text>}>
-                            </TabNavigator.Item>
-                        </TabNavigator>
-                    </View>
-                </View>
-                <View style={styles.bottomBox}>
-                    <View style={styles.bottom}>
-                        <View style={styles.views}>
-                            <TextInput
-                                style={styles.smallText}
-                                placeholder='Question Title'
-                                returnKeyType='next'
-                                placeholderTextColor='white'
-                                underlineColorAndroid='transparent' />
-                        </View>
-                        <View style={styles.views}>
-                            <TextInput
-                                style={styles.largeText}
-                                placeholder='Description'
-                                returnKeyType='next'
-                                placeholderTextColor='white'
-                                underlineColorAndroid='transparent'
-                                multiline={true} />
-                        </View>
-                        <View style={styles.button}>
-                            <Button title={'Ask Question'} onPress={this.onPressButton} />
-                        </View>
-                    </View>
-                </View>
-                <BottomNav tabNumber={2}/>
+                <ScrollableTabView style={styles.page}>
+                  <AskPage styles={styles.page} tabLabel={'Ask'} />
+                  <AnswerPage style={styles.page} tabLabel={'Answer'} />
+                </ScrollableTabView>
             </View>
         );
     }
@@ -73,24 +38,7 @@ export default class Ask extends Component {
 const styles = {
     backdrop: {
         backgroundColor: '#D3D3D3',
-        flex: 1
-    },
-    top: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1
-    },
-    logo: {
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    bar: {
-        alignSelf: 'stretch',
-        height: 50,
-        backgroundColor: '#fff',
-        opacity: 0.8,
-        flexDirection: 'row',
+        flex: 1,
     },
     text: {
         color: 'black',
@@ -103,43 +51,7 @@ const styles = {
         fontSize: 17,
         fontWeight: 'bold'
     },
-    bottom: {
-        backgroundColor: 'white',
-        borderRadius: 5,
+    page: {
+      flex: 1,
     },
-    bottomBox: {
-        paddingLeft: 10,
-        paddingRight: 10
-    },
-    views: {
-        paddingTop: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingBottom: 10,
-        borderBottomColor: '#D3D3D3',
-        borderBottomWidth: 1
-    },
-    button: {
-        paddingBottom: 10,
-        paddingTop: 15,
-    },
-    smallText: {
-        height: 50,
-        backgroundColor: '#DCDCDC',
-        borderWidth: 2,
-        borderColor: '#DCDCDC',
-        borderRadius: 5,
-        paddingRight: 25,
-        paddingLeft: 25,
-
-    },
-    largeText: {
-        height: 300,
-        backgroundColor: '#DCDCDC',
-        borderWidth: 2,
-        borderColor: '#DCDCDC',
-        borderRadius: 5,
-        paddingLeft: 10,
-        fontSize: 15
-    }
 }
